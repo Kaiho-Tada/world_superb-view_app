01fcd34acafdf4966569459527cd0e574645ac05
Kaiho-Tada <110219289+Kaiho-Tada@users.noreply.github.com>
2023-07-16 13:08:42 +0900

    Appコンポーネントの作成

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,15 +1,54 @@
-import { FC } from "react";
+import { createContext, FC, useEffect, useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import {Router} from "./router/Router"
 
-import theme from "theme/theme";
-import { Router } from "router/Router";
+import theme from "./theme/theme";
+import { getCurrentUser } from "lib/api/auth";
+import { User } from "types/api/user";
 
+export const AuthContext = createContext({} as {
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  isSignedIn: boolean
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+  currentUser: User | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+})
 const App: FC = () => {
+
+  const [loading, setLoading] = useState<boolean>(false)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  const handleGetCurrentUser = async () => {
+    setLoading(true)
+    try {
+      const res = await getCurrentUser()
+
+      if (res?.status === 200) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.currentUser)
+      } else {
+        console.log("No current user")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <Router />
+        <AuthContext.Provider value={{ loading, setLoading, isSignedIn, setIsSignedIn, currentUser, setCurrentUser}}>
+          <Router />
+        </AuthContext.Provider>
       </BrowserRouter>
     </ChakraProvider>
   )
